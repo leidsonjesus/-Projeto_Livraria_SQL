114801f32eb420ebdabc1ba73bce5e0adc842895
leidsonjesus <leidson18e@gmail.com>
2023-05-25 19:59:41 -0300

    ajuste na categorias dao

--- dao/categoria_dao.py
+++ dao/categoria_dao.py
@@ -8,7 +8,21 @@ class CategoriaDAO:
         self.__categorias: list[Categoria] = list()
 
     def listar(self) -> list[Categoria]:
-        return self.__categorias
+        categorias = list ()
+
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute("SELECT * FROM categorias")
+        resultados = cursor.fetchall()
+
+        for result in resultados: 
+            cat = Categoria(result[1])
+            cat.id = result[0]
+            categorias.append(cat)
+        cursor.close()
+        conexao.close() 
+
+        return categorias
 
     def adicionar(self, categoria: Categoria) -> None:
         conexao = self.__conexao_factory.get_conexao()
@@ -25,20 +39,34 @@ class CategoriaDAO:
     
 
     def remover(self, categoria_id: int) -> bool:
-        encontrado = False
-        for c in self.__categorias:
-            if (c.id == categoria_id):
-                index = self.__categorias.index(c)
-                self.__categorias.pop(index)
-                encontrado = True
-                break
-        return encontrado
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute("DELETE FROM categorias WHERE id = %s", (categoria_id,))
+
+        categoria_removidas = cursor.rowcount
+
+        conexao.commit()
+        cursor.close()
+        conexao.close()
+
+        if (categoria_removidas == 0 ):
+            return False
+        return True
 
     def buscar_por_id(self, categoria_id) -> Categoria:
         cat = None
-        for c in self.__categorias:
-            if (c.id == categoria_id):
-                cat = c
-                break
-        return cat
+
+        conexao = self.__conexao_factory.get_conexao()
+        cursor = conexao.cursor()
+        cursor.execute("SELECT id, nome FROM categorias WHERE id = %s", (categoria_id,))
+        resultado = cursor.fetchone()
+
+        if (resultado):
+            cat = Categoria(resultado[1])
+            cat.id = resultado[0]
+        
+        cursor.close()
+        conexao.close()
+
+        return cat 
     
